741c578ea3815f1322f11cd6070eac73ae678987
ArthurCarrerMedeiros <carrer.arthur@gmail.com>
2023-10-20 19:25:18 -0300

    Criando tabelas

---
+++ Tabelas.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE proc_trigger;
+
+CREATE TABLE cliente (
+	id INT AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    dt_nascimento DATETIME NOT NULL,
+    telefone VARCHAR(10) NOT NULL,
+    email VARCHAR(50),
+    CONSTRAINT ch_pk_cliente PRIMARY KEY (id)
+);
+
+CREATE TABLE vendedor (
+	id INT AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    dt_nascimento DATETIME NOT NULL,
+    telefone VARCHAR(10) NOT NULL,
+    email VARCHAR(50),
+    CONSTRAINT ch_pk_cliente PRIMARY KEY (id)
+);
